97fa611cd74ba1fa10726c676c565a69ec180825
SkalingClouds <chris.skaling@gmail.com>
2020-09-09 14:46:29 -0700

    a

--- New-RBACRoleFromSource.ps1
+++ New-RBACRoleFromSource.ps1
@@ -1,10 +1,8 @@
 param(
     [string]$ImportCsv
 )
-Write-Host "Importing Users..."
 $AllUsers = Import-Csv $ImportCsv
 $AllUsers | ForEach-Object {
-    Write-Host "Assigning RBAC Roles..."
     Write-Host $_.Username
     Get-AzADUser
     New-AzRoleAssignment -SignInName $_.Username -RoleDefinitionName $_.RoleDefinitionName -ResourceGroupName $_.ResourceGroupName -Verbose -Debug
